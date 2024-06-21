a210cd91b35d03a1656389ebb2b55a0f22953e63
Clare Codes <clarecodess@gmail.com>
2024-06-21 15:47:49 +0300

    updated colors

--- savannah/public/code_snippets/rick6.py
+++ savannah/public/code_snippets/rick6.py
@@ -1,13 +1,1 @@
-import requests
 
-url = "https://rickandmortyapi.com/api/episode"
-all_episodes = []
-
-while url:
-    response = requests.get(url)
-    data = response.json()
-    all_episodes.extend(data['results'])
-    url = data['info']['next']
-
-# Now all_episodes has all the episodes
-print(all_episodes)
