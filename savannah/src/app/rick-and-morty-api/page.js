ccab1b1f07e8508baf19f5e397128b167dd3d13e
Clare Codes <clarecodess@gmail.com>
2024-06-21 10:25:13 +0300

    added header and footer

--- savannah/src/app/rick-and-morty-api/page.js
+++ savannah/src/app/rick-and-morty-api/page.js
@@ -1,10 +1,17 @@
+// app/rick-and-morty-api/page.js
 import React from 'react';
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
 
 const RickAndMortyAPI = () => {
   return (
-    <div className="p-10">
-      <h1 className="text-3xl mb-4">Rick and Morty API</h1>
-      <p>Details about the Rick and Morty API.</p>
+    <div>
+      <Header />
+      <div className="p-10">
+        <h1 className="text-3xl mb-4">Rick and Morty API</h1>
+        <p>Details about the Rick and Morty API.</p>
+      </div>
+      <Footer />
     </div>
   );
 };
--- savannah/src/app/unsplash-api/page.js
+++ savannah/src/app/unsplash-api/page.js
@@ -1,10 +1,17 @@
+// app/unsplash-api/page.js
 import React from 'react';
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
 
 const UnsplashAPI = () => {
   return (
-    <div className="p-10">
-      <h1 className="text-3xl mb-4">Unsplash API</h1>
-      <p>Details about the Unsplash API.</p>
+    <div>
+      <Header />
+      <div className="p-10">
+        <h1 className="text-3xl mb-4">Unsplash API</h1>
+        <p>Details about the Unsplash API.</p>
+      </div>
+      <Footer />
     </div>
   );
 };
