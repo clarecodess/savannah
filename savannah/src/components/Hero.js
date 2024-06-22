c65cde274bd3c628af4b55ec82c92be0b4c72b0c
Clare Codes <clarecodess@gmail.com>
2024-06-22 16:35:46 +0300

    restyled

--- savannah/src/components/Hero.js
+++ savannah/src/components/Hero.js
@@ -15,19 +15,19 @@ const Hero = () => {
   };
 
   return (
-    <section className="bg-purple-800 text-lime-300 p-10 text-center">
-      <h2 className="text-2xl mb-8">Welcome to Unsplashed Ricky</h2>
-      <p>Discover how to use the Unsplash and Rick & Morty APIs for your projects.</p>
-      <div className="mt-8 space-x-4">
+    <section className="bg-gradient-to-r from-purple-800 to-indigo-800 text-lime-300 p-10 text-center">
+      <h2 className="text-4xl font-extrabold mb-8">Welcome to Unsplashed Ricky</h2>
+      <p className="text-lg mb-8">Discover how to use the Unsplash and Rick & Morty APIs for your projects.</p>
+      <div className="mt-8 flex justify-center space-x-4">
         <button
           onClick={navigateToUnsplash}
-          className="bg-cyan-400 hover:bg-lime-300 text-purple-800 font-bold py-2 px-4 rounded-full inline-block"
+          className="bg-cyan-400 hover:bg-lime-300 text-purple-800 font-bold py-2 px-4 rounded-full transition transform hover:scale-105"
         >
           Search for Images with Unsplash API
         </button>
         <button
           onClick={navigateToRickAndMorty}
-          className="bg-cyan-400 hover:bg-lime-300 text-purple-800 font-bold py-2 px-4 rounded-full inline-block"
+          className="bg-cyan-400 hover:bg-lime-300 text-purple-800 font-bold py-2 px-4 rounded-full transition transform hover:scale-105"
         >
           Get all Episodes with Rick and Morty API
         </button>
@@ -37,5 +37,3 @@ const Hero = () => {
 };
 
 export default Hero;
-
-
